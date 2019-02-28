1b4391df1ead9ce0b888482a5de4e620fde3d8e8
Radosław Wąsala <rwasala@future-processing.com>
2019-02-28 08:14:27 +0100

    Add CORS

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const app = express();
 app.use(bodyParser.json())
+app.use(cors());
+
 const mockData = {};
 
 const mockPort = getPortNumber(process.argv[2])
